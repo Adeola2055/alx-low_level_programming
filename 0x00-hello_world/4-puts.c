680f6325939018197cb4aa8ab238fd549013e7c7
Adeola2055 <iadeola2055@gmail.com>
2023-09-07 19:00:51 +0100

    my first c code on alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A program that uses the puts function
+* instead of the printf
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
