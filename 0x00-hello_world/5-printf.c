7222fe3c28f24547654e18bb6e68775ca8e0bdb4
Adeola2055 <iadeola2055@gmail.com>
2023-09-07 19:37:22 +0100

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: a program that use printf to
+* display a message to the stdout
+* Return: Always 0 success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
