22890e964c9be0e2b88b70514d084ddbd63da05d
Adeola2055 <iadeola2055@gmail.com>
2023-09-08 13:47:20 +0100

    modifield

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	srand(time(NULL));
+	int n = rand() % 201 - 100;
+
+	printf("%d\n", n);
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
