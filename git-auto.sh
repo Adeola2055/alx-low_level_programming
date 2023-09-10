4932091a1158eace261288ebfa018dd379030d2a
Adeola2055 <iadeola2055@gmail.com>
2023-09-10 04:30:53 +0100

    modifield

--- git-auto.sh
+++ git-auto.sh
@@ -3,7 +3,7 @@ read -p "Enter the name of directory or file you want to git add: " name
 git add $name
 read -p "Enter your commit message: " message
 git commit -m "$message"
-echo "\tDISPLAYING your GIT status!"
+echo -e "\tDISPLAYING your GIT status!"
 git status
-echo "\tPUSHING YOUR FILES INTO YOUR GITHUB REPOSITORY"
+echo -e "\tPUSHING YOUR FILES INTO YOUR GITHUB REPOSITORY"
 git push
