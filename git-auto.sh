d35000f0c7aa1a90a7e627f4ca5320d7e7d2f988
Adeola2055 <iadeola2055@gmail.com>
2023-09-10 05:50:55 +0100

    modified

--- git-auto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "Enter the name of directory or file you want to git add: " name
-git add $name
-read -p "Enter your commit message: " message
-git commit -m "$message"
-echo -e "\tDISPLAYING your GIT status!"
-git status
-echo -e "\tPUSHING YOUR FILES INTO YOUR GITHUB REPOSITORY"
-git push
