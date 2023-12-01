15d66ff8d7a1289a91f9bc48fd83639134b8d983
Adeola2055 <iadeola2055@gmail.com>
2023-12-01 07:41:35 +0100

    fix space error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar,\
+	const char *message = "and that piece of art is useful\" - Dora Korpar, \
 2015-10-19\n";
 	/* Using write system call to print to stderr (file descriptor 2) */
-	write(2, message, 59); /* 57 is the length of the message */
+	write(2, message, 60); /* 57 is the length of the message */
 	return (1);
 }
