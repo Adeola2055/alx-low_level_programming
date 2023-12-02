7f4c6575508f4a75f423dd8e10f5a4609a687db5
Adeola2055 <iadeola2055@gmail.com>
2023-12-02 16:48:18 +0100

    control structure

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ *  main -  Entry point
+ *
+ *  Return: Always (0) success
+ */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int n, n1;
+	int n, lastDigit;
 
-	srand(time(NULL));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	n1 = n % 10;
-
-	if (n1 > 5)
+	lastDigit = n % 10;
+	/* your code goes there */
+	printf("Last digit of %d is %d", n, lastDigit);
+	if (lastDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n1);
+		printf(" and is greater than 5\n");
 	}
-	else if (n1 == 0)
+	else if (lastDigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n1);
+		printf(" and is 0\n");
 	}
-	else if (n1 < 6)
+	else if (lastDigit < 6)
 	{
-		printf("Last digit is %d is %d and is less than 6 and not 0\n", n, n1);
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
