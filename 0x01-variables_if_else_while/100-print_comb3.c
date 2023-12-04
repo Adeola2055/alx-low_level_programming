9ee6e999db7160518cb1b84d469ace5ed7c6d461
Adeola2055 <iadeola2055@gmail.com>
2023-12-04 12:40:33 +0100

    fix: logic errors

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, y;
+
+	x = 0;
+
+	while (x <= 8)
+	{
+		y = 1;
+		while (y <= 9)
+		{
+			putchar(x + '0'); 
+			putchar(y + '0');
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
