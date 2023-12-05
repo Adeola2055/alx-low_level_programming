7c4888b0ff83207291acf0609e555af3274f77a9
Adeola2055 <iadeola2055@gmail.com>
2023-12-05 18:41:04 +0100

    functions palava 🥵🥵

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char message[] = "_putchar\n";
+	i = 0;
+
+	while (message[i] != '\0')
+	{
+		_putchar(message[i]);
+		i++;
+	}
+	return (0);
+}
