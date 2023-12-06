85f3480953a9b920c21563c567d35b5b9af4c1a4
Adeola2055 <iadeola2055@gmail.com>
2023-12-06 07:22:25 +0100

    creating user function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ *
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
