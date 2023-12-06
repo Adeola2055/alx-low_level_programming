b4c795b4922c8672629a611b216ae156a1d900ad
Adeola2055 <iadeola2055@gmail.com>
2023-12-06 15:15:02 +0100

    on god

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit of a number
+ * @num: argument of print_last_digit().
+ *
+ * Return: last digit of num.
+ */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
