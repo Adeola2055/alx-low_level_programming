b29b10e412fdff761e47f29f31c01f3c04ee43a6
Adeola2055 <iadeola2055@gmail.com>
2023-12-07 13:38:08 +0100

    avant

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print all natural numbers from n to 98
+ * @n: Number
+ *
+ * Return: Void
+ */
+void sub(int n);
+
+void print_to_98(int n) {
+    if (n <= 98) {
+        while (n <= 98) {
+            sub(n);
+            if (n != 98) {
+                printf(", ");
+            }
+            n++;
+        }
+    } else {
+        while (n >= 98) {
+            sub(n);
+            if (n != 98) {
+                printf(", ");
+            }
+            n--;
+        }
+    }
+    printf("\n");
+}
+
+/**
+ * sub - check if a number is negative and handle digit that a    in't single.
+ * @n: number.
+ * Return: Void
+ */
+
+void sub(int n) {
+    if (n < 0) {
+        printf("-");
+        n = -n;
+    }
+
+    /* Handle the case where the number has more than one digit */
+    if (n >= 10) {
+        printf("%d%d", n / 10, n % 10);
+    } else {
+        printf("%d", n);
+    }
+}
