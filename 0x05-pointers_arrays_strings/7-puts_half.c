d945169950c77b53f000b6f775505df8bc49f75b
Adeola2055 <iadeola2055@gmail.com>
2023-12-14 17:12:01 +0100

    cyrus fucking storm

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ void puts_half(char *str)
 		length++;
 	}
 	/* check if half of str is odd or even */
-	if (length % 2 == 0)
+	/*if (length % 2 == 0)
 	{
 		n = length / 2;
 	}
-	else
-		n = (length - 1) / 2;
+	else */
+	n = (length + 1) / 2;
 	/* print the second half of str */
 	while (n < length)
 	{
