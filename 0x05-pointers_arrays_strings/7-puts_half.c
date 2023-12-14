b04c8e25ae7ff430aadf1a34e96e0f945ed41363
Adeola2055 <iadeola2055@gmail.com>
2023-12-14 16:52:54 +0100

    cyrus fucking storm

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints the second of a string
+ * @str: operand
+ */
+void puts_half(char *str)
+{
+	int n;
+	int length = 0;
+
+	/* get length of str */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	/* check if half of str is odd or even */
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+	}
+	else
+		n = (length - 1) / 2;
+	/* print the second half of str */
+	while (n < length)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
