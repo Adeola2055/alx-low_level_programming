05551f754f44633ac72c16a202b0475db95657b0
Adeola2055 <iadeola2055@gmail.com>
2023-12-21 17:17:27 +0100

    ragnar

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,28 +8,37 @@
 char *leet(char *str)
 {
 	int index = 0;
+	char current_char;
 
 	while (str[index] != '\0')
 	{
-		if (str[index] == 'a' || str[index] == 'A')
+		current_char = str[index];
+		if ((current_char == 'a' || current_char == 'A') ||
+			(current_char == 'e' || current_char == 'E') ||
+			(current_char == 'o' || current_char == 'O') ||
+			(current_char == 't' || current_char == 'T') ||
+			(current_char == 'l' || current_char == 'L'))
 		{
-			str[index] = '4';
-		}
-		else if (str[index] == 'e' || str[index] == 'E')
-		{
-			str[index] = '3';
-		}
-		else if (str[index] == 'o' || str[index] == 'O')
-		{
-			str[index] = '0';
-		}
-		else if (str[index] == 't' || str[index] == 'T')
-		{
-			str[index] = '7';
-		}
-		else if (str[index] == 'l' || str[index] == 'L')
-		{
-			str[index] = '1';
+			if (str[index] == 'a' || str[index] == 'A')
+			{
+				str[index] = '4';
+			}
+			else if (str[index] == 'e' || str[index] == 'E')
+			{
+				str[index] = '3';
+			}
+			else if (str[index] == 'o' || str[index] == 'O')
+			{
+				str[index] = '0';
+			}
+			else if (str[index] == 't' || str[index] == 'T')
+			{
+				str[index] = '7';
+			}
+			else if (str[index] == 'l' || str[index] == 'L')
+			{
+				str[index] = '1';
+			}
 		}
 		index++;
 	}
