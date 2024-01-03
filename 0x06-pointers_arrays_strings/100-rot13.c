1275fd6b4b37a0a88bb8541b4d4dc289e3fddc06
Adeola2055 <iadeola2055@gmail.com>
2024-01-03 11:48:42 +0100

    keep going

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string rot13
+ * @str: string to be encode
+ *
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	char original[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotated[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int index = 0;
+
+	while (*str)
+	{
+		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
+		{
+			index = 0;
+			while (*str != original[index])
+			{
+				index++;
+			}
+			*str = rotated[index];
+		}
+		str++;
+	}
+	return (str);
+}
