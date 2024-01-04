7890ac2376cbb1602a9a5b34d4102edb326b1b84
Adeola2055 <iadeola2055@gmail.com>
2024-01-04 16:20:20 +0100

    2055

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - fills the first n bytes of the memory area pointed 
+ * to by s with the constant byte b.
+ * @s: pointer to a memory address
+ * @b: constant byte
+ * @n: number of bytes
+ *
+ * Return: a pointer to s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+	return (s);
+}
