07979326f615422f5bf08de0485f96d3d5295de0
Adeola2055 <iadeola2055@gmail.com>
2024-01-05 14:46:11 +0100

    trying to fix checkers error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,12 @@
 char *_strchr(char *s, char c)
 {
 	int index = 0;
-	char *res;
 
 	while (*s)
 	{
 		if (s[index] == c)
 		{
-			res = &s[index];
-			return (res);
+			return (&s[index]);
 		}
 		index++;
 	}
