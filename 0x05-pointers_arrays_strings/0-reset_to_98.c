3bd43019b9b406e249dc4c226ad9541501c7c0ef
Adeola2055 <iadeola2055@gmail.com>
2024-01-07 10:36:39 +0100

    keep going

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - takesa pointer and updates the value it points to.
+ * reset_to_98 - takes a pointer and updates the value it points to.
  * @n: a pointer to a value
  */
 void reset_to_98(int *n)
