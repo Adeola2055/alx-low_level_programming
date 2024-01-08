cfea3e7c2217239e73820c289bedccc624240223
Adeola2055 <iadeola2055@gmail.com>
2024-01-08 12:22:54 +0100

    🤗

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string.
+ * @str: pointer to a string value
+ */
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
