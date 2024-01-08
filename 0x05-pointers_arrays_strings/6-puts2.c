db190263b79e0f6de88181b5e5cc1ef73f08a3f4
Adeola2055 <iadeola2055@gmail.com>
2024-01-08 12:31:00 +0100

    🤗

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,15 @@
  */
 void puts2(char *str)
 {
-	while (*str)
+	int index = 0;
+
+	while (str[index] != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if (index == 0 || index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+		index++;
 	}
 	_putchar('\n');
 }
