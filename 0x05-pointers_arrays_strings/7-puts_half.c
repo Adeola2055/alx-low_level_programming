c904863fef62d25b2eb3297ebd1c60342cf2e5c7
Adeola2055 <iadeola2055@gmail.com>
2024-01-08 13:32:40 +0100

    cyrus fucking storm

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,7 @@ void puts_half(char *str)
 	{
 		length++;
 	}
-	if (length % 2 != 0)
-	{
-		n = (length - 1) / 2;
-	}
-	else
-	{ 
-		n = length / 2;
-	}
+	n = length / 2;
 	index = n;
 	while (str[index] != '\0')
 	{
