0981326fcaae2b4a2d318a9993dace6f1e8d6748
Adeola2055 <iadeola2055@gmail.com>
2024-01-08 13:26:19 +0100

    cyrus fucking storm

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of a string.
+ * @str: a pointer to a string value
+ */
+void puts_half(char *str)
+{
+	int n, index, length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 != 0)
+	{
+		n = (length - 1) / 2;
+	}
+	else
+	{ 
+		n = length / 2;
+	}
+	index = n;
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+}
