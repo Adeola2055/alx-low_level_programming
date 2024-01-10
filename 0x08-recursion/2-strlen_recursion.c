ca28408f77f10087e97cc3bdb57bc9b9b04f2106
Adeola2055 <iadeola2055@gmail.com>
2024-01-10 22:23:19 +0100

    on god

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: pointer to string
+ *
+ * Return: length 
+ */
+int _strlen_recursion(char *s)
+{
+    /* Base Case: Check if the current character is the null character */
+    if (*s == '\0')
+    {
+        return 0; /* Length is 0 when the end of the string is reached */
+    }
+    /* Recursive Call: Move to the next character in the string */
+    return 1 + _strlen_recursion(s + 1);
+}
