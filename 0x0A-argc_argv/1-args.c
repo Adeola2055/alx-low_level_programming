afd94412c8d65e54d12616e2641367fe69e65a69
Adeola2055 <iadeola2055@gmail.com>
2024-01-13 03:03:38 +0100

    ace

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * _count: print number of cmd argument
+ * @argc: number of cmd argument
+ */
+void _count(int argc)
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < (argc - 1))
+		{
+			count++;
+		}
+	}
+	printf("%d\n", count);
+}
+/**
+ * main - print a number
+ * @argc: number of cmd argument
+ * @argv: unsued variable
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	_count(argc);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 void _print_filename(char *argv[]);
+void _count(int argc);
 
 #endif
