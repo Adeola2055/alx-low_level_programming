d1b1b5e0fa0530c95bc19eb2d541a128ab9556c2
Adeola2055 <iadeola2055@gmail.com>
2024-01-16 08:06:38 +0100

    DND

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - copy the string given as a parameter
+ * @str: pointer to a string value
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int length = strlen(str), i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ptr = (char *) malloc(length * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < length)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
