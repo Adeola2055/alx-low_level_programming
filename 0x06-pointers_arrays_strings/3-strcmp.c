111c5b0b2af2953063cac208ed4095cfab5b76e0
Adeola2055 <iadeola2055@gmail.com>
2024-01-17 12:55:15 +0100

    revision

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+/**
+ * _strcmp - compares two string
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: 0 if both strings are same ordiffernces in ascii values otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	/* compare each string */
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		return (*s1 - *s2);
+	}
+	/* check if both strings have reach null terminator */
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0); /* both strings are equal */
+	}
+	/* if one string is shorter than the other */
+	return (*s1 - *s2);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * reverse_array - reverse content of an array of integers
+ * @a: pointer to an array of integer
+ * @n: number of elements of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, r2 = n - 1;
+	char temp;
+	while (i < r2)
+	{
+		temp = a[i];
+		a[i] = a[r2];
+		a[r2] = temp;
+		i++;
+		r2--;
+	}
+}
