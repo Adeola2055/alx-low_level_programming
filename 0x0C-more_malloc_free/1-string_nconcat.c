9eefe38e9a28476cac4b548993882fb552f6ce21
Adeola2055 <iadeola2055@gmail.com>
2024-01-23 17:03:07 +0100

    never

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,32 +10,40 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	int len1, i = 0;
+	char *ptr;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
-
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	}
+	if (n >= strlen(s2))
+	{
+		n = strlen(s2);
+	}
+	len1 = strlen(s1);
+	ptr = (char *) malloc((n + len1 + 1) * sizeof(*ptr));
+	if (ptr == NULL)
+	{
 		return (NULL);
-
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+	}
+	while (*s1)
+	{
+		*ptr = *s1;
+		ptr++;
+		s1++;
+	}
+	while (*s2 && i < n)
+	{
+		*ptr = *s2;
+		ptr++;
+		s2++;
+		i++;
+	}
+	ptr[len1 + 1 + n] = '\0';
+	return (ptr);
 }
