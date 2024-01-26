cf143d2605fbe72ac0d1581a03b6a5b3cb41ae33
Adeola2055 <iadeola2055@gmail.com>
2024-01-26 07:06:27 +0100

    is yours

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: points to an array that contains the dog name
+ * @age: points to an array that contains the dog age
+ * @owner: points to an array that contains the dog oner name
+ *
+ */
 struct dog
 {
 	char *name;
