7d0921fae5b1bba2e9bebf0a2b6b3fbd8371bd76
Adeola2055 <iadeola2055@gmail.com>
2024-01-26 13:47:09 +0100

    ace

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <sttdef.h>
+/**
+ * free_dog - frees dog
+ * @d: points to a struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
